93d5a255b30fdc6069718a65d803d1ff46749640
Thanh Long <btlong2610@gmail.com>
2025-06-20 17:45:26 +0700

    medium

---
+++ 2294. Partition Array Such That Maximum Difference Is K.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int partitionArray(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int cnt=1;
+        int tmp=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]-tmp>k){
+                ++cnt;
+                tmp=nums[i];
+            }
+        }
+
+        return cnt;
+    }
+};
